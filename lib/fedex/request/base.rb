281c25a4d09d4bd1d285e178b4d65a6f9c184939
Carlos Vilhena <carlosvilhena@gmail.com>
2013-10-17 15:43:42 +0100

    include service type if it exists

--- lib/fedex/request/base.rb
+++ lib/fedex/request/base.rb
@@ -96,9 +96,7 @@ module Fedex
       def add_requested_shipment(xml)
         xml.RequestedShipment{
           xml.DropoffType @shipping_options[:drop_off_type] ||= "REGULAR_PICKUP"
-          unless service_type.nil? or service_type.empty?
-            xml.ServiceType service_type
-          end
+          xml.ServiceType service_type
           xml.PackagingType @shipping_options[:packaging_type] ||= "YOUR_PACKAGING"
           add_shipper(xml)
           add_recipient(xml)
